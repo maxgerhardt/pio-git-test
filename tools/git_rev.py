2f9490f6b90c58a17654d8b6c23155b641cf089c
unknown <maximilian.gerhardt@rub.de>
2022-09-09 23:28:32 +0200

    fully working

--- tools/git_rev.py
+++ tools/git_rev.py
@@ -19,7 +19,7 @@ branchcmd = "git rev-parse --abbrev-ref HEAD"
 branch = subprocess.check_output(branchcmd, shell=True).decode().strip()
 
 # Make all available for use in the macros
-print("-DPIO_SRC_NAM={0}".format(project))
-print("-DPIO_SRC_TAG={0}".format(version))
-print("-DPIO_SRC_REV={0}".format(commit))
-print("-DPIO_SRC_BRH={0}".format(branch))
+print("'-DPIO_SRC_NAM=\"{0}\"'".format(project))
+print("'-DPIO_SRC_TAG=\"{0}\"'".format(version))
+print("'-DPIO_SRC_REV=\"{0}\"'".format(commit))
+print("'-DPIO_SRC_BRH=\"{0}\"'".format(branch))
